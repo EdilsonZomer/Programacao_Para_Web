047bca96571adc32f389c518e020e8120c0bfc45
ChristianoGrabovski <c.grabovski@gmail.com>
2018-11-24 13:45:09 -0200

    função de adiconar ao carrinho pronta

--- Java_Script_Js/AdicionarAoCarrinho.js
+++ Java_Script_Js/AdicionarAoCarrinho.js
@@ -1,4 +1,11 @@
 function AdicionarAoCarrinho(nome, preco, quantidade) {
+    for( var i in carrinho){
+        if(carrinho[i].nome === nome){
+            carrinho[i].quantidade ++;
+            return;
+    }
+}
+
     var joguinho = new Jogo(nome, preco, quantidade)
     carrinho.push(joguinho)
     }
